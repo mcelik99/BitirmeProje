4d393d2309f1d7efd379571526f8988b7d267948
mcelik99 <mehmetcelik.99@outlook.com>
2023-05-20 18:48:55 +0300

    period controller

--- BitirmeProjesi/Areas/Admin/Controllers/PeriodController.cs
+++ BitirmeProjesi/Areas/Admin/Controllers/PeriodController.cs
@@ -1,5 +1,7 @@
 ﻿using BitirmeProjesi.Data;
+using BitirmeProjesi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BitirmeProjesi.Areas.Admin.Controllers
 {
@@ -12,11 +14,46 @@ namespace BitirmeProjesi.Areas.Admin.Controllers
             this.BitirmeDBContext = BitirmeDBContext;
         }
 
+
+
+        // Yeni bir Period oluşturur
+        [HttpPost]
+        public ActionResult Create(Period period)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                BitirmeDBContext.Periods.Add(period);
+
+               
+                BitirmeDBContext.SaveChanges();
+                
+            }
+
+            return View(period);
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+           var period = BitirmeDBContext.Periods.FirstOrDefault(p => p.Id == id);
+            if (period == null)
+            {
+                return NotFound();
+            }
+
+            return View(period);
+        }
+
+
         public IActionResult Index()
         {
-            int UserID = 1;
+             int UserID = 1;
 
-            return View();
+            var periods = BitirmeDBContext.Periods.ToList();
+            return View(periods);
         }
+
+
     }
 }
