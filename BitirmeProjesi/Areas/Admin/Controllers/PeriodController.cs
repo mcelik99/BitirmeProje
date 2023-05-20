b6a1b6804f45893ba7d0760ee4457eee9ed0c1f6
mcelik99 <mehmetcelik.99@outlook.com>
2023-05-20 16:10:30 +0300

    Period List

---
+++ BitirmeProjesi/Areas/Admin/Controllers/PeriodController.cs
@@ -0,0 +1,22 @@
+﻿using BitirmeProjesi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitirmeProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PeriodController : Controller
+    {
+        private readonly BitirmeDBContext BitirmeDBContext;
+        public PeriodController(BitirmeDBContext BitirmeDBContext)
+        {
+            this.BitirmeDBContext = BitirmeDBContext;
+        }
+
+        public IActionResult Index()
+        {
+            int UserID = 1;
+
+            return View();
+        }
+    }
+}
