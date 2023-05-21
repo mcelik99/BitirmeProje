2d4c0147da465f7b943fd286dbd75a78efad3816
mcelik99 <mehmetcelik.99@outlook.com>
2023-05-21 15:17:06 +0300

    Student Area Login Check

---
+++ BitirmeProjesi/Areas/Student/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BitirmeProjesi.Areas.Student.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var ID = this.HttpContext.Session.GetInt32("STUDENT_ID");
+            if (Convert.ToInt32(ID) == 0)
+            {
+                /*  context.Result = new RedirectToRouteResult(
+                       new RouteValueDictionary
+                       {
+                          {"controller", "Login"},
+                          {"action", "Student"}
+                       }
+                  );*/
+
+                context.Result = new RedirectResult("/Login/Student");
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
