c7ead9cd4125b1e799d4559dfbe953716b54026b
mcelik99 <mehmetcelik.99@outlook.com>
2023-05-21 12:37:20 +0300

    User Crud And Views

---
+++ BitirmeProjesi/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,170 @@
+﻿using BitirmeProjesi.Data;
+using BitirmeProjesi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitirmeProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly BitirmeDBContext _context;
+        private IPasswordHasher<User> PasswordHasher;
+
+        public UserController(BitirmeDBContext context, IPasswordHasher<User> passwordHasher)
+        {
+            this.PasswordHasher = passwordHasher;
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            List<User> user = await _context.Users.ToListAsync();
+            return View(user);
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Surname,PasswordHash,Email,PhoneNumber,IsAdvisor,Quota")] User user)
+        {
+
+          
+            if (ModelState.IsValid)
+            {
+                
+                user.UserName = user.Email;
+                user.EmailConfirmed = true;
+                user.PhoneNumberConfirmed = true;
+
+                user.NormalizedUserName = user.UserName.ToUpper();
+                user.NormalizedEmail = user.Email.ToUpper();
+
+              
+
+                user.PasswordHash = this.PasswordHasher.HashPassword(user , user.PasswordHash);
+
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Entry(user).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            User user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(u => u.Id == id);
+        }
+    }
+}
