0d966fc82f2d20791a19b7cb0142dc4a94426c3b
mcelik99 <mehmetcelik.99@outlook.com>
2023-05-28 14:13:38 +0300

    Akademisyen sayfası

---
+++ BitirmeProjesi/Areas/Admin/Controllers/AcademianController.cs
@@ -0,0 +1,58 @@
+﻿using BitirmeProjesi.Data;
+using BitirmeProjesi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitirmeProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AcademianController : Controller
+    {
+        private readonly BitirmeDBContext _context;
+
+        public AcademianController(BitirmeDBContext context)
+        {
+
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            User CurrentUser = _context.Users.Where(x => x.UserName == this.HttpContext.User.Identity.Name).FirstOrDefault();
+
+
+            string Sql = "Select Periods.* from Periods Left Join Participants on Participants.PeriodId = Periods.Id Left Join ParticipantTeachers on ParticipantTeachers.ParticipantId = Participants.Id Where ParticipantTeachers.TeacherId =  " + CurrentUser.Id;
+
+            List<Period> result = _context.Periods.FromSqlRaw(Sql).ToList();
+
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Period period = await _context
+                .Periods
+                .Include(p => p.CreateUser)
+                .Include(p => p.Participants)
+                .Include("Participants.Student")
+                .Include("Participants.ParticipantTeachers")
+                .Include("Participants.ParticipantTeachers.Teacher")
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (period == null)
+            {
+                return NotFound();
+            }
+
+            return View(period);
+        }
+    }
+}
