588ab00cb1e02947df69efbedf7c108d43d17bbc
mcelik99 <mehmetcelik.99@outlook.com>
2023-06-14 23:30:51 +0300

    no message

--- BitirmeProjesi/Areas/Student/Controllers/ChatController.cs
+++ BitirmeProjesi/Areas/Student/Controllers/ChatController.cs
@@ -1,26 +1,27 @@
-﻿using Azure.Core;
-using BitirmeProjesi.Data;
+﻿using BitirmeProjesi.Data;
 using BitirmeProjesi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace BitirmeProjesi.Areas.Admin.Controllers
+namespace BitirmeProjesi.Areas.Student.Controllers
 {
     [Area("Admin")]
     [Authorize]
-    public class ChatController : Controller
+    public class ChatController : BaseController
     {
         private readonly BitirmeDBContext _context;
 
         public ChatController(BitirmeDBContext context)
         {
+
             _context = context;
         }
 
         public IActionResult Index(int id)
         {
-            User CurrentUser = _context.Users.Where(x => x.UserName == this.HttpContext.User.Identity.Name).FirstOrDefault();
+           
+
 
             Chat chat = this._context.Chats
                 .Include("Student")
@@ -30,21 +31,6 @@ namespace BitirmeProjesi.Areas.Admin.Controllers
                 .Include("ChatMessages.Student")
                 .Where(x => x.UserId == CurrentUser.Id && x.StudentId == id)
                 .FirstOrDefault();
-
-            if (chat == null)
-            {
-                chat = new Chat();
-                chat.CreateAt = DateTime.Now;
-                chat.StudentId = id;
-                chat.UserId = CurrentUser.Id;
-
-                this._context.Chats.Add(chat);
-
-                this._context.SaveChanges();
-
-                return RedirectToAction("Create", new { id = id });
-            }
-
 
             return View(chat);
         }
