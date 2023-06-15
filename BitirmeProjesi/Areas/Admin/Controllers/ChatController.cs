a5117607a6427145c181cb806a8d78b485c01b6f
mcelik99 <mehmetcelik.99@outlook.com>
2023-06-15 20:37:56 +0300

    Akademisyen  ChatController Update

--- BitirmeProjesi/Areas/Admin/Controllers/ChatController.cs
+++ BitirmeProjesi/Areas/Admin/Controllers/ChatController.cs
@@ -4,6 +4,8 @@ using BitirmeProjesi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
 
 namespace BitirmeProjesi.Areas.Admin.Controllers
 {
@@ -57,8 +59,43 @@ namespace BitirmeProjesi.Areas.Admin.Controllers
             _context.Add(chatMessage);
             _context.SaveChanges();
 
+            // Mesaj gönderildikten sonra e-posta gönderimi
+            Chat chat = _context.Chats.FirstOrDefault(x => x.Id == chatMessage.ChatId);
+            User user = _context.Users.FirstOrDefault(x => x.Id == chat.UserId);
+            BitirmeProjesi.Models.Student student = _context.Students.FirstOrDefault(x => x.Id == chat.StudentId);
+
+            string subject = "Yeni Mesaj";
+            string body = $"Hocanız size yeni bir mesaj gönderdi: {chatMessage.MessageBody}";
+
+            SendEmail(student.Email, subject, body);
+
             return Redirect(Request.Headers["Referer"].ToString());
         }
+
+        private void SendEmail(string emailAddress, string subject, string body)
+        {
+            string smtpHost = "smtp.gmail.com"; // SMTP sunucu adresi
+            int smtpPort = 587; // SMTP sunucu portu
+            string smtpUsername = "bitirmeornek@gmail.com"; // SMTP sunucu kullanıcı adı
+            string smtpPassword = "rnloubgenjjjksax"; // SMTP sunucu şifresi
+
+            using (var client = new SmtpClient(smtpHost, smtpPort))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                client.EnableSsl = true;
+
+                using (var message = new MailMessage(smtpUsername, emailAddress))
+                {
+                    message.Subject = subject;
+                    message.Body = body;
+                    message.IsBodyHtml = false;
+
+                    client.Send(message);
+                }
+            }
+        }
+
     }
 
 }
